8ac103ba724a0e953a75d33e428260b49521cd4f
Thuraiya <3eya151@gmail.com>
2025-04-22 09:33:01 +0400

    Evaluate Postfix Expression

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿namespace StakQueueAndFilesTask
+﻿using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+
+namespace StakQueueAndFilesTask
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            string expression = "5 3 + 8 *";
+            int result = EvaluatePostfixExpression(expression);
+            Console.WriteLine($"Result: {expression} {result}");
+        }
+
+        static void ReverseAStringUsingStack(string[] args)
         {
             //Reverse a String Using Stack
             Console.Write("Enter world to reverse: ");
@@ -20,16 +30,52 @@
             {
                 revers += stack.Pop();
             }
-
             Console.WriteLine("Reversed string: " + revers);
+        }
 
-        
+        static int EvaluatePostfixExpression(string expression)
+        {
+            Stack<int> stack = new Stack<int>();
+            string[] tokens = expression.Split(' ');
 
+            foreach (string token in expression.Split(' '))
+            {
+                if (int.TryParse(token, out int number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+
+                    int b = stack.Pop();
+                    int a = stack.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            stack.Push(a + b);
+                            break;
+                        case "-":
+                            stack.Push(a - b);
+                            break;
+                        case "*":
+                            stack.Push(a * b);
+                            break;
+                        case "/":
+                            stack.Push(a / b);
+                            break;
+                        default:
+                            throw new InvalidOperationException($"Unknown operator: {token}");
+                    }
+                }
+            }
+
+            return stack.Pop();
         }
 
 
-
-    }
-
+        }            
 }
+
+
     
